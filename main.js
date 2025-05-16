d68f112d5790fb22fbb9a58ce2b447c6d57a2491
Emmanuel Alo <emmanuelalo209@gmail.com>
2025-05-16 20:43:37 +0800

    finalized

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import express from 'express'; //server
-import bodyParser from 'body-parser'; //Middleware - parsing (optional, you use express.json())
+import bodyParser from 'body-parser'; //Middleware 
 import mysql from 'mysql2'; // database
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; // jwt token
@@ -28,7 +28,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-const SECRET_KEY = 'your_secret_key_here'; // Use env var in production!
+const SECRET_KEY = process.env.SECRET_KEY;
 
 // Database connection
 const db = mysql.createConnection({
@@ -144,33 +144,6 @@ app.delete('/posts/:id', authenticateToken, (req, res) => {
 
 
   
-// let posts = [
-//     {
-//         title: "How to Start a Tech Blog in 2025",
-//         content: "Learn how to launch a tech blog by picking a niche and writing consistently.",
-//         author: "Jane Martinez"
-//         },
-//       {
-//         title: "Top 10 VS Code Extensions for Developers",
-//         content: "Boost your coding workflow with popular VS Code extensions like Prettier and GitLens.",
-//         author: "Daniel Park"
-//       },
-//       {
-//         title: "The Future of Web Development: Trends to Watch",
-//         content: "Explore new trends like serverless, WebAssembly, and Svelte shaping web development.",
-//         author: "Fatima Noor"
-//       },
-//       {
-//         title: "Writing Clean Code: Best Practices",
-//         content: "Use clear names, small functions, and avoid repeated code to write clean code.",
-//         author: "Liam Chen"
-//       },
-//       {
-//         title: "Deploying Your Blog with Netlify and GitHub",
-//         content: "Host your blog for free by connecting your GitHub repo to Netlify.",
-//         author: "Amira Johnson"
-//       }
- 
-// ]
+
 
 
